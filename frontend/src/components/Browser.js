cc121ae7caa863c715b52d1fe686e2d6a0db1f98
Hapiel <daniel@danielsiegmund.nl>
2020-10-29 02:26:10 +0100

    New results display

--- frontend/src/components/Browser.js
+++ frontend/src/components/Browser.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Api from '../util/Api';
 import ValidationError from '../util/ValidationError';
-import { ChromePicker } from 'react-color';
 import Color from './Color';
+import '../CSS/browser.css';
 
 export default function Browser() {
     const [state, setState] = useState([]);
@@ -15,18 +15,37 @@ export default function Browser() {
     return (
         <>
             <Color/>
+            <div className="flex-container">
             <ShowArtworks/>  
+            </div>
         </>
     )
 
+
+
     function ShowArtworks() {
+        
         if (isLoading) {
-            return <h1>Loading...</h1>
+            return <h3>Loading...</h3>
         } 
         return state.map((image, i) =>
-            <div key={i}>
-                <h1>{image.title}</h1>
-                <img src={image.url} alt={image.title}></img>
+            <div key={i} id={"div"+i} className="pixel-result" style={{maxWidth: Math.max(300, image.width) }}>
+                <table>
+                    <tbody>
+                    <tr><td>Title:</td><td><strong>{image.title}</strong></td></tr>
+                    <tr><td>Author:</td><td><strong>{image.author}</strong></td></tr>
+                    <tr><td>Date:</td><td>{new Date(image.date).toLocaleDateString()}</td></tr>
+                    <tr><td style={{whiteSpace: "nowrap"}}>Color count:</td><td>{image.trans ? image.colorCount + 1: image.colorCount}</td></tr>
+                    <tr><td>Size:</td><td>{image.width} x {image.height}</td></tr>
+                    </tbody>
+                </table>
+                <div className="artwork">
+                    <img src={image.url} alt={image.title} id={i}></img>
+                </div>
+                <div className="zoom-buttons">
+                    <button type="button" onClick={()=>zoomIn(i)}>+</button>
+                    <button type="button" onClick={()=>zoomOut(i)}>-</button>
+                </div>
             </div> 
         );
     }
@@ -49,4 +68,22 @@ export default function Browser() {
 
         Api.get('/api/yellow', validator, onSucces, onFailure)
     }
+
+    function zoomIn(id) {
+        var img = document.getElementById(id);
+        var div = document.getElementById("div" + id);
+        var width = img.clientWidth;
+        var divWidth = div.clientWidth;
+        if (divWidth < width * 2) div.style.maxWidth = (width * 2) + "px";
+        img.style.width = (width * 2) + "px";
+        console.log("zooming");
+    }
+
+    function zoomOut(id) {
+        var img = document.getElementById(id);
+        var width = img.clientWidth;
+        if (width === img.naturalWidth) return false;
+        img.style.width = (width / 2) + "px";
+    }
+
 }
