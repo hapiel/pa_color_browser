a8b4aea1de00d45e671a731a26a7d018d364a4d6
Marinus <marinusvandenoever@gmail.com>
2020-11-02 12:20:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const imagesRouter = require('./routes/images');
 app.use('/api', imagesRouter);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/frontend/build'))
+  res.sendFile(path.resolve(__dirname + '/frontend/build/index.html'))
 })
 
 app.listen(port, () => {
